19cb79e32cf5c375d1f56e8b00dd196f15bb3bd5
imkp32 <58550808+imkp32@users.noreply.github.com>
2021-01-25 12:06:30 +0530

    Add files via upload

---
+++ 22.1.21 JS/scriptloop.js
@@ -0,0 +1,242 @@
+const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+const newArr = []
+for(let i = 0; i < countries.length; i++){
+  newArr.push(countries[i].toUpperCase())
+}
+console.log(newArr)
+//# pattern//
+first='#'
+for(let i=1;i<=7;i++)
+{
+  let last=first.repeat(i)
+  console.log(last)
+  
+}
+//function//
+function square() {
+  let num = 2
+  let sq = num * num
+  console.log(sq)
+}
+
+square() // 4
+//
+function sumAllNums() {
+  let sum = 0
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i]
+  }
+  return sum
+}
+
+console.log(sumAllNums(1, 2, 3, 4))
+//
+const sumAll=(...arguments)=>{
+  let sum=0
+  for(const element of arguments)
+  {
+    sum+=element
+  } 
+  return sum
+}
+console.log(sumAll(1,2,3,4,5))
+//
+const changeToUpperCase = arr => {
+  const newArr = []
+  for (const element of arr) {
+    newArr.push(element.toUpperCase())
+  }
+  return newArr
+}
+
+const countriess = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland']
+console.log(changeToUpperCase(countriess))
+//
+function greetings(name = 'Peter') {
+  let message = `${name}, welcome to 30 Days Of JavaScript!`
+  return message
+}
+
+console.log(greetings())
+console.log(greetings('Asabeneh'))
+//
+function weightOfObject(mass, gravity = 9.81) {
+  let weight = mass * gravity + ' N' // the value has to be changed to string first
+  return weight
+}
+
+console.log('Weight of an object in Newton: ', weightOfObject(100)) // 9.81 gravity at the surface of Earth
+console.log('Weight of an object in Newton: ', weightOfObject(100, 1.62))
+//
+const rectangle = {
+  length: 20,
+  width: 20
+}
+console.log(rectangle) // {length: 20, width: 20}
+
+/*const person = {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+  age: 250,
+  country: 'Finland',
+  city: 'Helsinki',
+  skills: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Node',
+    'MongoDB',
+    'Python',
+    'D3.js'
+  ],
+  isMarried: true
+}
+console.log(person.city)*/
+/*const users = {
+  Alex: {
+    email: 'alex@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript'],
+    age: 20,
+    isLoggedIn: false,
+    points: 30
+  },
+  Asab: {
+    email: 'asab@asab.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+    age: 25,
+    isLoggedIn: false,
+    points: 50
+  },
+  Brook: {
+    email: 'daniel@daniel.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+    age: 30,
+    isLoggedIn: true,
+    points: 50
+  },
+  Daniel: {
+    email: 'daniel@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  },
+  John: {
+    email: 'john@john.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+    age: 20,
+    isLoggedIn: true,
+    points: 50
+  },
+  Thomas: {
+    email: 'thomas@thomas.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  },
+  Paul: {
+    email: 'paul@paul.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  }
+}
+let count=0;
+for(var i=0;i<users.length;i++)
+{
+    if(users[i].isLoggedIn)
+      {
+          count++
+      }
+}
+console.log(count)
+let newArrr=[]
+for(var i=0;i<users.length;i++)
+{
+    const values=Object.values(users[i].skills)
+    newArrr.push(values.length)
+}
+console.log(Math.max(newArrr))*/
+//
+const users = [
+  {
+      _id: 'ab12ex',
+      username: 'Alex',
+      email: 'alex@alex.com',
+      password: '123123',
+      createdAt:'08/01/2020 9:00 AM',
+      isLoggedIn: false
+  },
+  {
+      _id: 'fg12cy',
+      username: 'Asab',
+      email: 'asab@asab.com',
+      password: '123456',
+      createdAt:'08/01/2020 9:30 AM',
+      isLoggedIn: true
+  },
+  {
+      _id: 'zwf8md',
+      username: 'Brook',
+      email: 'brook@brook.com',
+      password: '123111',
+      createdAt:'08/01/2020 9:45 AM',
+      isLoggedIn: true
+  },
+  {
+      _id: 'eefamr',
+      username: 'Martha',
+      email: 'martha@martha.com',
+      password: '123222',
+      createdAt:'08/01/2020 9:50 AM',
+      isLoggedIn: false
+  },
+  {
+      _id: 'ghderc',
+      username: 'Thomas',
+      email: 'thomas@thomas.com',
+      password: '123333',
+      createdAt:'08/01/2020 10:00 AM',
+      isLoggedIn: false
+  }
+  ];
+
+  
+let count=0
+for(var i=0;i<users.length;i++)
+{
+  if(users[i].isLoggedIn)
+      count++
+}
+console.log(count)
+//
+const numbers = [1, 2, 3, 4]
+​
+const sumArray = arr => {
+  let sum = 0
+  const callBack = function(element) {
+    sum += element
+  }
+  numbers.forEach(callback)
+  return sum
+
+}
+console.log(sumArray(numbers))
+//
+const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const products = [
+  { product: 'banana', price: 3 },
+  { product: 'mango', price: 6 },
+  { product: 'potato', price: ' ' },
+  { product: 'avocado', price: 8 },
+  { product: 'coffee', price: 10 },
+  { product: 'tea', price: '' },
+]
+
+const newSyll=names.map((namee)=>console.log(namee.toUpperCase()))
+console.log(newSyll)
